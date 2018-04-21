3b2d803a409ee96acf27ff7f96744e25ad1b24f8
Sohel Shekha <sohelshekha@Sohels-MacBook-Pro.local>
2018-04-21 14:17:50 +0800

    Adding search

--- Dialogs/BasicLuisDialog.cs
+++ Dialogs/BasicLuisDialog.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Configuration;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 using Microsoft.Bot.Builder.Dialogs;
@@ -30,7 +32,8 @@ namespace Microsoft.Bot.Sample.LuisBot
         [LuisIntent("Greeting")]
         public async Task GreetingIntent(IDialogContext context, LuisResult result)
         {
-            await this.ShowLuisResult(context, result);
+            await context.PostAsync("Hello there, nice to meet you");
+            context.Wait(MessageReceived);
         }
 
         [LuisIntent("Cancel")]
@@ -43,6 +46,43 @@ namespace Microsoft.Bot.Sample.LuisBot
         public async Task HelpIntent(IDialogContext context, LuisResult result)
         {
             await this.ShowLuisResult(context, result);
+        }
+
+        [LuisIntent("Entertainment.Search")]
+        public async Task SearchIntent(IDialogContext context, LuisResult result)
+        {
+            string query = "";
+            string imageURL = "http://www.theoldrobots.com/images62/Bender-18.JPG";
+
+            if (result.Entities != null && result.Entities.Count > 0)
+            {
+                query = result.Entities[0].Entity;
+
+                //call API to get robohash image
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Add("X-Mashape-Key", "E2FDfvmYYNmshbiepUhzSpBNkoW5p1Eif8Tjsnf84Ux8VnEGoR");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync("https://robohash.p.mashape.com/index.php?text=" + query);
+                if (response.IsSuccessStatusCode)
+                {
+                    dynamic obj = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(await response.Content.ReadAsStringAsync());
+                    imageURL = obj["imageUrl"];
+                }
+            }
+
+            //create new message, with image
+            var msg = context.MakeMessage();
+            msg.Text = "Here you go: " + query;
+            msg.Attachments = new List<Connector.Attachment>()
+            {
+                new Connector.Attachment("image/jpeg",imageURL)
+            };
+
+            //send message
+            await context.PostAsync(msg);
+            context.Wait(MessageReceived);
         }
 
         private async Task ShowLuisResult(IDialogContext context, LuisResult result) 
