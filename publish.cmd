216a6ea18ca57cce4040ddd46d0b06429acead79
Sohel Shekha <sohelshekha@Sohels-MacBook-Pro.local>
2018-04-21 14:10:40 +0800

    Initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=gabrobo-Web-Deploy.pubxml -p:Password=S2oyNKEmhfvdZTebylaNcjkyulsvFvN5u1rkt4zmZg1067MrvsqkernlS28A
+
